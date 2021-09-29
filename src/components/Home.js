78d31c6008a6fffdabe300c3b843bd180f4c73f9
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-09-29 19:45:43 +0100

    Style the whole project

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+/* eslint-disable semi */
+import React from 'react';
+import '../style/Home.css';
+
+const Home = () => (
+  <div className="home">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrub exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ipsu
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrub exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ipsu
+    </p>
+  </div>
+);
+
+export default Home;
